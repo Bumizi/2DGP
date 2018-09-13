133f18c7f2fd04ef0761dbaea5efcbcaa38be3d0
jang hyurk youn(c dond 220 - 25) <trg0317@naver.com>
2018-09-13 17:14:57 +0900

    dril 04 완성
    2014180011 김영범
    https://www.youtube.com/watch?v=LKHim_VDYlA&feature=youtu.be

---
+++ Drils/Dril_04/Lecture04/character_runs.py
@@ -0,0 +1,33 @@
+from pico2d import *
+open_canvas()
+grass = load_image('grass.png')
+character = load_image('animation_sheet.png')
+
+x = 0
+direction = 0
+frame = 0
+while True:
+    while direction == 0:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame+1) % 8
+        x += 5
+        delay(0.05)
+        get_events()
+        if x >= 800:
+            direction = 1
+    while direction == 1:
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x -= 5
+        delay(0.05)
+        get_events()
+        if x <= 0:
+            direction = 0
+close_canvas()
+
