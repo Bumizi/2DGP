61cb1a46a788350f17e694065ab423ec9b5910e1
Bumizi <43131635+Bumizi@users.noreply.github.com>
2018-09-17 20:49:34 +0900

    Dril_05 2차
    첫번째 점에서 두번째 점까지 이동 시뮬레이션 완성. 두 좌표의 거리를 40 만큼 나누고 x증가량과 y증가량을 구해 나눈 횟수만큼 더해주는 방식으로 이동. 인덱스를 증가시켜 다음 점으로 넘어가도록 해야함

--- Drils/Dril_05/dril_05.py
+++ Drils/Dril_05/dril_05.py
@@ -3,57 +3,68 @@ import math
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
 Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92],
-              [692, 518], [682, 336], [712, 349]]
+              [692, 518], [682, 336], [712, 349], [203, 535]]
 
+
+def move_right_face(dif_x, dif_y, i):
+    x = Point_List[i][0]
+    y = Point_List[i][1]
+    frame = 0
+    j = 0
+    while j != 40:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += dif_x
+        y += dif_y
+        j += 1
+        delay(0.05)
+        get_events()
+    pass
+
+#x != Point_List[i+1][0] & y != Point_List[i+1][1]
+
+def move_left_face(dif_x, dif_y, i):
+    x = Point_List[i][0]
+    y = Point_List[i][1]
+    frame = 0
+    j = 0
+    while j != 40:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += dif_x
+        y += dif_y
+        j += 1
+        delay(0.05)
+        get_events()
+    pass
+
+
+def move_point_to_point(i):
+    dif_x = (Point_List[i+1][0]-Point_List[i][0]) // 40
+    dif_y = (Point_List[i+1][1]-Point_List[i][1]) // 40
+    if dif_x > 0:
+        move_right_face(dif_x, dif_y, i)
+    elif dif_x < 0:
+        move_left_face(dif_x, dif_y, i)
+
+    pass
+
+
+#character.draw_now(Point_List[i][0], Point_List[i][1])
 i = 0
-x = 0
-y = 0
-direction = 0
-frame = 0
-dif_x = 0
-dif_y = 0
-
-def move_right_face():
-    x = 0
-    frame = 0
-    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
-    update_canvas()
-    frame = (frame + 1) % 8
-    x += 5
-    delay(0.05)
-    get_events()
-    pass
-def move_left_face():
-    x = 0
-    frame = 0
-    clear_canvas()
-    grass.draw(400, 30)
-    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
-    update_canvas()
-    frame = (frame + 1) % 8
-    x -= 5
-    delay(0.05)
-    get_events()
-    pass
-
-def move_point_to_point():
-    x, y = Point_List[i]
-    dif_x=(Point_List[i][1]-Point_List[i][1])
-    dif_y=(Point_List[i+1][1]-Point_List[i+1][1])
-    distance = math.sqrt((dif_x**2)+(dif_y**2)) / 40
-    pass
-
 while True:
-     clear_canvas_now()
-     grass.draw_now(400, 30)
-     character.draw_now(Point_List[i][0], Point_List[i][1])
-     move_point_to_point()
-     if dif_x > 0:
-         move_right_face()
-     elif dif_x < 0:
-         move_left_face()
-    
+    if i < 10:
+     i += 1
+    elif i >= 10:
+     i = 0
+     move_point_to_point(i)
 close_canvas()
