dd4336bb82573587bab36d66c545cd932f54e815
Bumizi(Class) <43131635+Bumizi@users.noreply.github.com>
2018-09-17 16:47:50 +0900

    dril 05 최종 완성 (연습문제)
    사각형 그리기 + 원 그리기 완성

--- Drils/Dril_05/dril_05.py
+++ Drils/Dril_05/dril_05.py
@@ -49,11 +49,11 @@ def move_from_left_to_center():
 import math
 
 def make_rectangle():
-    #move_from_center_to_right()
-    #move_up()
-    #move_left()
-    #move_down()
-    #move_from_left_to_center()
+    move_from_center_to_right()
+    move_up()
+    move_left()
+    move_down()
+    move_from_left_to_center()
     pass
 
 def make_circle():
