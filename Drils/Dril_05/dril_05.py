8f92e0369ee08e550a7cb4613643786c27f517a8
Bumizi(Class) <43131635+Bumizi@users.noreply.github.com>
2018-09-17 17:35:07 +0900

    Dril_05 1차
    좌표들을 List로 저장하고 좌표간의 변화량을 구하여 왼쪽, 오른쪽 방향을 정함. 후에 이동하는 기능 구현 예정

--- Drils/Dril_05/dril_05.py
+++ Drils/Dril_05/dril_05.py
@@ -1,76 +1,59 @@
 from pico2d import *
-
+import math
 open_canvas()
 
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-def move_from_center_to_right():
-    x, y = 800 // 2, 90
-    while x < 800 - 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
-def move_up():
-    x, y = 800 -25, 90
-    while y < 600 - 50:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y += 2
-        delay(0.01)
-def move_left():
-    x, y = 800 - 25, 600 - 50
-    while x > 0 + 25:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x -= 2
-        delay(0.01)
-def move_down():
-    x, y = 0 + 25, 600 - 50
-    while y > 0 + 90:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        y -= 2
-        delay(0.01)
-def move_from_left_to_center():
-    x, y = 0 + 25, 0 + 90
-    while x < 800 // 2:
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        x += 2
-        delay(0.01)
+Point_List = [[203, 535], [132, 243], [535, 470], [477, 203], [715, 136], [316, 225], [510, 92],
+              [692, 518], [682, 336], [712, 349]]
 
-import math
+i = 0
+x = 0
+y = 0
+direction = 0
+frame = 0
+dif_x = 0
+dif_y = 0
 
-def make_rectangle():
-    move_from_center_to_right()
-    move_up()
-    move_left()
-    move_down()
-    move_from_left_to_center()
+def move_right_face():
+    x = 0
+    frame = 0
+    character.clip_draw(frame * 100, 100, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x += 5
+    delay(0.05)
+    get_events()
+    pass
+def move_left_face():
+    x = 0
+    frame = 0
+    clear_canvas()
+    grass.draw(400, 30)
+    character.clip_draw(frame * 100, 0, 100, 100, x, 90)
+    update_canvas()
+    frame = (frame + 1) % 8
+    x -= 5
+    delay(0.05)
+    get_events()
     pass
 
-def make_circle():
-    cx, cy, r = 800 // 2, 600 // 2, (600-180)//2
-    degree = -90
-    while degree < 270:
-        radian = math.radians(degree)
-        x = cx + r * math.cos(radian)
-        y = cy + r * math.sin(radian)
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        degree += 1
-        delay(0.01)
+def move_point_to_point():
+    x, y = Point_List[i]
+    dif_x=(Point_List[i][1]-Point_List[i][1])
+    dif_y=(Point_List[i+1][1]-Point_List[i+1][1])
+    distance = math.sqrt((dif_x**2)+(dif_y**2)) / 40
+    pass
 
 while True:
-    make_rectangle()
-    make_circle()
+     clear_canvas_now()
+     grass.draw_now(400, 30)
+     character.draw_now(Point_List[i][0], Point_List[i][1])
+     move_point_to_point()
+     if dif_x > 0:
+         move_right_face()
+     elif dif_x < 0:
+         move_left_face()
     
 close_canvas()
