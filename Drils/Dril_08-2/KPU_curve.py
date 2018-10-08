d275b90791990a22b18217ddfcf3df168fee3c27
Bumizi <bumizi95@naver.com>
2018-10-08 13:24:06 +0900

    Dril_08(1,2)
    2014180011 김영범

---
+++ Drils/Dril_08-2/KPU_curve.py
@@ -0,0 +1,45 @@
+from pico2d import *
+import random
+
+KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+
+open_canvas(KPU_WIDTH, KPU_HEIGHT)
+character = load_image('animation_sheet.png')
+kpu_ground = load_image('KPU_GROUND.png')
+
+frame = 0
+motion = 100  # 캐릭터의 이동방향을 표현하는 스프라이트 시트의 y값
+x = 25
+y = 50
+
+
+def draw_curve_4_points(p1, p2, p3, p4):
+    global frame, tx, x, y, motion
+    # draw p2-p3
+    for i in range(0, 100, 2):
+        tx = x
+        t = i / 100
+        x = ((-t ** 3 + 2 * t ** 2 - t) * p1[0] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[0] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[0] + (t ** 3 - t ** 2) * p4[0]) / 2
+        y = ((-t ** 3 + 2 * t ** 2 - t) * p1[1] + (3 * t ** 3 - 5 * t ** 2 + 2) * p2[1] + (
+                    -3 * t ** 3 + 4 * t ** 2 + t) * p3[1] + (t ** 3 - t ** 2) * p4[1]) / 2
+        clear_canvas()
+        if x < tx:
+            motion = 0
+        elif x > tx:
+            motion = 100
+        kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+        character.clip_draw(frame * 100, motion, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        delay(0.05)
+
+
+points = [(random.randint(0 + 50, KPU_WIDTH - 50), random.randint(0 + 50, KPU_HEIGHT - 50)) for i in range(10)]
+i = 0
+
+while 1:
+    draw_curve_4_points(points[i - 3], points[i - 2], points[i - 1], points[i])
+    i = (i + 1) % 10
+
+close_canvas()
